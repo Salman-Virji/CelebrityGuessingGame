e60012936b8abefbd8069d4bfc6aaa424037ea64
Salman Virji <salmanvirji@gmail.com>
2022-03-17 17:42:18 -0600

    Game screen logic

--- app/src/main/java/com/salmanvirji/celebrityguessinggame/Game.java
+++ app/src/main/java/com/salmanvirji/celebrityguessinggame/Game.java
@@ -3,16 +3,31 @@ package com.salmanvirji.celebrityguessinggame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
-public class Game extends AppCompatActivity {
-    String JSON_STRING = "{\"celebs\":{\"name1\":\"Bobert\",\"name2\":\"John\",\"name3\":\"Smith\",\"name4\":\"Bob\",\"correct\":\"Salman\"}}";
 
-    String n1,n2,n3,n4,correct;
-    Button btnChoice1, btnChoice2, btnChoice3, btnChoice4;
+public class Game extends AppCompatActivity implements View.OnClickListener  {
+
+    ArrayList<String> celebList = new ArrayList<>();
+
+    Button btnChoice1, btnChoice2, btnChoice3, btnChoice4,reset;
+    TextView txtScorenum;
+    String correctname ="correctname";
+   // String celebname;
+    int index = 0;
+
 
 
     @Override
@@ -24,29 +39,131 @@ public class Game extends AppCompatActivity {
         btnChoice2 = (Button) findViewById(R.id.btnChoice2);
         btnChoice3 = (Button) findViewById(R.id.btnChoice3);
         btnChoice4 = (Button) findViewById(R.id.btnChoice4);
+        reset= findViewById(R.id.idReset);
 
-        //https://abhiandroid.com/programming/json
-        try{
-            JSONObject obj = new JSONObject(JSON_STRING);
-            JSONObject celebs = obj.getJSONObject("celebs");
-            n1=celebs.getString("name1");
-            n2=celebs.getString("name2");
-            n3=celebs.getString("name3");
-            n4=celebs.getString("name4");
-            correct=celebs.getString("correct");
+        btnChoice1.setOnClickListener(this);
+        btnChoice2.setOnClickListener(this);
+        btnChoice3.setOnClickListener(this);
+        btnChoice4.setOnClickListener(this);
+        reset.setOnClickListener(this);
 
-            btnChoice1.setText(n1);
-            btnChoice2.setText(n2);
-            btnChoice3.setText(n3);
-            btnChoice4.setText(n4);
-        }
-        catch(JSONException e){
-            e.printStackTrace();
-        }
+
+        txtScorenum = findViewById(R.id.txtScorenum);
+        //gameStart();
+        getCelebs();
+
+
+
 
 
 
     }
+
+    // https://www.youtube.com/watch?v=h71Ia9iFWfI tutorial on importing and parsing json data
+    private void getJSON(String correctname, String celebname)
+    {
+
+        String json;
+        try{
+            InputStream is = getAssets().open("celebs.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+
+            is.read(buffer);
+            is.close();
+
+            json = new String(buffer,"UTF-8");
+
+            JSONArray jsonArray = new JSONArray(json);
+
+            for(int i = 0; i<jsonArray.length(); i++){
+                JSONObject obj = jsonArray.getJSONObject(i);
+                if(obj.getString(correctname).equals(celebname)){
+
+                    celebList.add(obj.getString("name1"));
+                    celebList.add(obj.getString("name2"));
+                    celebList.add(obj.getString("name3"));
+                    celebList.add(obj.getString("name4"));
+
+
+
+                }
+            }
+
+
+
+
+        }catch (IOException | JSONException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void getCelebs(){
+
+        celebList.clear();
+        String[] name={"Ryan Renolds","Danny Devito","Steve Buscemi"};
+        getJSON(correctname,name[index].toString());
+        btnChoice1.setText(celebList.get(0).toString());
+        btnChoice2.setText(celebList.get(1).toString());
+        btnChoice3.setText(celebList.get(2).toString());
+        btnChoice4.setText(celebList.get(3).toString());
+
+
+
+    }
+
+
+    public void onClick(View v){
+
+
+        int i = 0;
+        //int index;
+        int score = 0;
+        String s;
+        Button clickedBtn = findViewById(v.getId());
+        String[] name={"Ryan Renolds","Danny Devito","Steve Buscemi"};
+
+
+
+
+
+            if(clickedBtn.getText().equals("Next")){
+
+                if(index >=0 && index <=3){
+                    index++;
+                }
+                if(index ==3){
+                    index = 0;
+                }
+                getCelebs();
+            }
+
+
+            if(clickedBtn.getText().equals(name[index]) && !clickedBtn.getText().equals("Next") )
+            {
+                score++;
+                s =String.valueOf(score);
+                txtScorenum.setText(s);
+                clickedBtn.setText("Winner");
+
+
+
+            }
+            else if(!clickedBtn.equals(name[index])&& !clickedBtn.getText().equals("Next")){
+                clickedBtn.setText("Wrong");
+
+               // getCelebs();
+            }
+
+
+
+
+
+
+
+    }
+
+
 
 }
 
